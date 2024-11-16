34306ec1974ea8a97755b0c1f9681a84fd6fa2a4
Cuong <21020285@vnu.edu.vn>
2024-11-17 00:47:43 +0700

    update header form when resize window and clear unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Header } from './component/Header';
 
 function App() {
--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,30 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import '../styles/Header.css';
 import { Logo } from './Logo';
 
 export const Header = () => {
-  const [collapsed, setCollapsed] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      const width = window.innerWidth;
-      if (width > 576 && width < 768) {
-        setCollapsed(true);
-      } else if (width >= 768) {
-        setCollapsed(false);
-      }
-
-      return width;
-    };
-
-    handleResize();
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   return (
-    <div className="flex items-center min-h-16 h-16 ml-8">
+    <div className="flex items-center min-h-16 h-16 ml-8 max-md:max-w-full">
       {/* Logo */}
       <div className="mr-2">
         <Logo size={40} stroke="white" />
