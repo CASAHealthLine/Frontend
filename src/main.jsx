69289b0fe7e74a0290db5dc250e27d1fef5331d5
Cuong <21020285@vnu.edu.vn>
2024-12-13 16:31:14 +0700

    improve code

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <Router>
         <Routes>
-          <Route path="/login" element={<App />} />
+          <Route path="/login" element={<Entry />} />
           <Route path="/register" element={<Entry />} />
 
           <Route path="*" element={<App />} />
